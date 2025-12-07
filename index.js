306f469e5e312fbfa4ed435c20188030393b3bd1
GOD-Da <dawiniefleury17@gmail.com>
2025-12-07 02:00:06 -0500

    Refactor index.js for improved structure and routing

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/index.html')
-});
-
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
+app.use('/code', code);
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+// Optional: si ou vle redirect root '/' sou /pair
+app.get('/', (req, res) => {
+    res.redirect('/pair');
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star ‼️
-
-
-Server running on http://localhost:` + PORT)
+Server running on http://localhost:` + PORT);
 });
 
 module.exports = app;
